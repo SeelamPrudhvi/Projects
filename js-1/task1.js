25b91530d598587462633097fbc50255b12e3754
SeelamPrudhvi <prudhvikumarseelam@gmail.com>
2023-09-13 19:25:44 +0530

    javascript1 commit

---
+++ js-1/task1.js
@@ -0,0 +1,8 @@
+//swap
+var name1 = "Prudhvi"; //container1
+var name2 = "Gouthami"; //container2
+//interchange?
+var name3 = name2;
+var name4 = name3 + " " + name1;
+console.log("before swap:", name1, name2); //before swap
+console.log("after swap:", name4); //after swap
